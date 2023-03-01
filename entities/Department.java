4a5c2973b35d151d07e6e760934ee4e5f919a5f6
Adriano Rodrigues <o.adrian.rodrigues@gmail.com>
2023-03-01 13:43:45 -0300

    First commit

---
+++ entities/Department.java
@@ -0,0 +1,23 @@
+package entities;
+
+public class Department {
+	private String name;
+
+	
+	public Department() {
+		super();
+	}
+
+	public Department(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
